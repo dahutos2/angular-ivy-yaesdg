4d47ed2c0d7b7eebd9d8efa481a50598642e9112
dahutos2 <dahutos2@gmail.com>
2022-07-27 19:51:26 +0900

    3 files modified

--- src/app/buy/buy.component.ts
+++ src/app/buy/buy.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Item } from '../item';
 import { ItemService } from '../item.service';
+import { MatButtonToggleAppearance } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-buy',
@@ -8,16 +9,30 @@ import { ItemService } from '../item.service';
   styleUrls: ['./buy.component.css'],
 })
 export class BuyComponent implements OnInit {
+  @Input() appearance: MatButtonToggleAppearance;
   items: Item[] = [];
+  text: string = 'トグルボタン未選択';
+  value: number;
 
   constructor(private itemService: ItemService) {}
 
   ngOnInit(): void {
     this.getItems();
     console.log(this.items);
+    this.value = 0;
   }
 
   getItems(): void {
     this.itemService.getItems().subscribe((items) => (this.items = items));
+  }
+
+  tglClick(tglValue: number) {
+    if (this.value === tglValue) {
+      this.value = 0;
+      this.text = 'トグルボタン未選択';
+    } else {
+      this.value = tglValue;
+      this.text = '左から' + this.value + 'つ目を選択中';
+    }
   }
 }
